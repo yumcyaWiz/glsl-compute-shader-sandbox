86400277cfe8195aab33f598f5381492d8c7803a
yumcyawiz <hashdolf1923@gmail.com>
2021-12-31 01:23:24 +0900

    basic setup

---
+++ sandbox/mandelbrot/src/main.cpp
@@ -0,0 +1,91 @@
+#include <cstdio>
+#include <iostream>
+
+#include "glad/gl.h"
+//
+#include "GLFW/glfw3.h"
+//
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+static void glfwErrorCallback(int error, const char* description) {
+  fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+}
+
+int main() {
+  const uint32_t width = 512;
+  const uint32_t height = 512;
+
+  // init glfw
+  glfwSetErrorCallback(glfwErrorCallback);
+  if (!glfwInit()) {
+    return -1;
+  }
+
+  // init window and OpenGL context
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);  // required for Mac
+  glfwWindowHint(GLFW_SAMPLES, 4);                      // 4x MSAA
+  GLFWwindow* window =
+      glfwCreateWindow(width, height, "atmos", nullptr, nullptr);
+  if (!window) {
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+  glfwSwapInterval(1);  // enable vsync
+
+  // init glad
+  if (!gladLoadGL((GLADloadfunc)glfwGetProcAddress)) {
+    std::cerr << "failed to initialize OpenGL context" << std::endl;
+    return -1;
+  }
+
+  // init imgui
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO& io = ImGui::GetIO();
+  (void)io;
+
+  // set imgui style
+  ImGui::StyleColorsDark();
+
+  // init imgui backends
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplOpenGL3_Init("#version 460 core");
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+
+    // start imgui frame
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    ImGui::Begin("UI");
+    ImGui::End();
+
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // render imgui
+    ImGui::Render();
+    int display_w, display_h;
+    glfwGetFramebufferSize(window, &display_w, &display_h);
+    glViewport(0, 0, display_w, display_h);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    glfwSwapBuffers(window);
+  }
+
+  // cleanup
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+
+  return 0;
+}
