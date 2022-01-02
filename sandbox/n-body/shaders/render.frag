377262f148e83ed22a56fc88ef43d491aa1cb1be
yumcyawiz <hashdolf1923@gmail.com>
2022-01-02 13:14:14 +0900

    more nice init

--- sandbox/n-body/shaders/render.frag
+++ sandbox/n-body/shaders/render.frag
@@ -5,6 +5,10 @@ out vec4 fragColor;
 void main() {
   vec2 uv = 2.0 * gl_PointCoord - vec2(1);
   float r = length(uv);
-  vec3 color = r < 1.0 ? 0.01 / r * vec3(1, 235.0 / 255, 209.0 / 255) : vec3(0);
+  vec3 color = r < 1.0 ? 0.01 / r * vec3(0.6, 0.8, 1.0) : vec3(0);
+
+  // reinhard
+  color = color / (vec3(1) + color);
+
   fragColor = vec4(color, 1.0);
 }
--- sandbox/n-body/src/renderer.h
+++ sandbox/n-body/src/renderer.h
@@ -76,11 +76,23 @@ class Renderer {
     std::mt19937 mt(rnd_dev());
     std::uniform_real_distribution<float> dist(-1, 1);
 
+    const int grid_size = std::sqrt(nParticles);
     std::vector<Particle> data(nParticles);
-    for (std::size_t i = 0; i < data.size(); ++i) {
-      data[i].position = 2.0f * glm::vec4(dist(mt), dist(mt), dist(mt), 0);
-      data[i].velocity = 0.1f * glm::vec4(dist(mt), dist(mt), dist(mt), 0);
-      data[i].mass = 10000.0f * 0.5f * (dist(mt) + 1.0f);
+    for (std::size_t idx = 0; idx < data.size(); ++idx) {
+      const int i = idx % grid_size;
+      const int j = (idx / grid_size) % grid_size;
+      const float u = (2.0f * i) / grid_size - 1.0f;
+      const float v = (2.0f * j) / grid_size - 1.0f;
+
+      const glm::vec3 position =
+          glm::vec3(u, v, 0) + 0.0f * glm::vec3(dist(mt), dist(mt), dist(mt));
+      const glm::vec3 velocity =
+          0.2f * glm::cross(glm::normalize(-position), glm::vec3(0, 0, 1));
+
+      data[idx].position = glm::vec4(position, 0);
+      data[idx].velocity = glm::vec4(velocity, 0);
+      data[idx].mass = 10000.0f * 0.5f * (dist(mt) + 1.0f);
+      // data[idx].mass = 10000.0f;
     }
 
     particlesIn.setData(data, GL_DYNAMIC_DRAW);
