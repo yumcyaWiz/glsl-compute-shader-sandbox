6adb9d9b4fde4fa3ff02b314c3c15142b411a7e4
yumcyawiz <hashdolf1923@gmail.com>
2022-01-04 20:42:43 +0900

    hold texture object inside GaussianBlur

--- sandbox/n-body/src/gaussian-blur.h
+++ sandbox/n-body/src/gaussian-blur.h
@@ -8,30 +8,36 @@ using namespace gcss;
 class GaussianBlur {
  private:
   ComputeShader shader;
+  Texture texOut;
 
  public:
-  GaussianBlur() {
+  GaussianBlur() : texOut{glm::uvec2(512, 512), GL_RGBA32F, GL_RGBA, GL_FLOAT} {
     shader.setComputeShader(std::filesystem::path(CMAKE_CURRENT_SOURCE_DIR) /
                             "shaders" / "gaussian-blur.comp");
     shader.linkShader();
   }
-  ~GaussianBlur() { shader.destroy(); }
 
-  void blur(const Texture& tex_in) const {
-    // prepare output texture
+  void destroy() {
+    shader.destroy();
+    texOut.destroy();
+  }
+
+  void blur(const Texture& tex_in) {
+    // detect resolution change
     const glm::uvec2 resolution = tex_in.getResolution();
-    Texture tex_out{resolution, tex_in.getInternalFormat(), tex_in.getFormat(),
-                    tex_in.getType()};
+    if (texOut.getResolution() != resolution) {
+      texOut.setResolution(resolution);
+    }
 
     // run two pass gaussian blur
     // blur horizontally
     tex_in.bindToImageUnit(0, GL_READ_ONLY);
-    tex_out.bindToImageUnit(1, GL_WRITE_ONLY);
+    texOut.bindToImageUnit(1, GL_WRITE_ONLY);
     shader.setUniform("horizontal", true);
     shader.run(std::ceil(resolution.x / 8.0f), std::ceil(resolution.y / 8.0f),
                1);
     // blur vertically
-    tex_out.bindToImageUnit(0, GL_READ_ONLY);
+    texOut.bindToImageUnit(0, GL_READ_ONLY);
     tex_in.bindToImageUnit(1, GL_WRITE_ONLY);
     shader.setUniform("horizontal", false);
     shader.run(std::ceil(resolution.x / 8.0f), std::ceil(resolution.y / 8.0f),
