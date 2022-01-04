4efbb4f02ac9d7dc1be46232fd6cb3ed3d269501
yumcyawiz <hashdolf1923@gmail.com>
2022-01-04 22:50:59 +0900

    impl bloom class

---
+++ sandbox/n-body/src/bloom.h
@@ -0,0 +1,59 @@
+#ifndef _BLOOM_H
+#define _BLOOM_H
+#include "gaussian-blur.h"
+#include "gcss/shader.h"
+#include "gcss/texture.h"
+
+class Bloom {
+ private:
+  GaussianBlur blur;
+
+  Texture fragColor;
+  Texture brightColor;
+  ComputeShader extractBrightColor;
+
+  ComputeShader additiveBlend;
+
+ public:
+  Bloom()
+      : fragColor{glm::uvec2(512, 512), GL_RGBA32F, GL_RGBA, GL_FLOAT},
+        brightColor{glm::uvec2(512, 512), GL_RGBA32F, GL_RGBA, GL_FLOAT} {
+    extractBrightColor.setComputeShader(
+        std::filesystem::path(CMAKE_CURRENT_SOURCE_DIR) / "shaders" /
+        "extract-bright-color.comp");
+    additiveBlend.setComputeShader(
+        std::filesystem::path(CMAKE_CURRENT_SOURCE_DIR) / "shaders" /
+        "additive-blend.comp");
+  }
+
+  void bloom(const Texture& tex_in) {
+    // detect resolution change
+    const glm::uvec2 resolution = tex_in.getResolution();
+    if (fragColor.getResolution() != resolution) {
+      fragColor.setResolution(resolution);
+    }
+    if (brightColor.getResolution() != resolution) {
+      brightColor.setResolution(resolution);
+    }
+
+    // extract bright color
+    tex_in.bindToImageUnit(0, GL_READ_ONLY);
+    fragColor.bindToImageUnit(1, GL_WRITE_ONLY);
+    brightColor.bindToImageUnit(2, GL_WRITE_ONLY);
+    extractBrightColor.setUniform("threshold", 1.0f);
+    extractBrightColor.run(std::ceil(resolution.x / 8.0f),
+                           std::ceil(resolution.y / 8.0f), 1);
+
+    // gaussian blur bright color texture
+    blur.blur(brightColor);
+
+    // additive blending
+    fragColor.bindToImageUnit(0, GL_READ_ONLY);
+    brightColor.bindToImageUnit(1, GL_READ_ONLY);
+    tex_in.bindToImageUnit(2, GL_WRITE_ONLY);
+    additiveBlend.run(std::ceil(resolution.x / 8.0f),
+                      std::ceil(resolution.y / 8.0f), 1);
+  }
+};
+
+#endif
